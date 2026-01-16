080c23afcac302b71e55b81eb6baaa52a6db4c9b
Simollino <simone.alocchi@studio.unibo.it>
2026-01-16 18:17:16 +0100

    updated bootstrap.php

--- templates/bootstrap.php
+++ templates/bootstrap.php
@@ -1,8 +1,30 @@
 <?php
+// Avvia sessione e fornisce $db_obj e il link al CSS (minimale, modifica credenziali DB se necessario)
+if (session_status() === PHP_SESSION_NONE) {
     session_start();
+}
 
-    require_once("utils/functions.php");
-    require_once("db/database.php");
+// carica lo style del progetto
+echo '<link rel="stylesheet" href="/css/style.css">' . PHP_EOL;
 
-    $db_obj = new Database("localhost", "root", "", "campus_oggetti_smarriti", 3306);
+// Inizializza l'oggetto DB se non esiste già
+if (!isset($db_obj)) {
+    // include la classe Database (assume esiste c:\xampp\htdocs\lostandfound\db\database.php)
+    require_once __DIR__ . '/../db/database.php';
+
+    // Parametri di default per XAMPP locale; se il vostro team usa altri valori, cambiateli qui
+    $dbHost = '127.0.0.1';
+    $dbUser = 'root';
+    $dbPass = '';
+    $dbName = 'campus_oggetti_smarriti';
+    $dbPort = 3306;
+
+    // Crea l'istanza (se la classe Database ha firma diversa, adattare)
+    try {
+        $db_obj = new Database($dbHost, $dbUser, $dbPass, $dbName, $dbPort);
+    } catch (Throwable $e) {
+        // non interrompere con errore visibile al frontend: $db_obj rimane non settato
+        $db_obj = null;
+    }
+}
 ?>
