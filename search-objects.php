fb2f348f09249d949e3b08f88c4ec9e2ac199d18
Simollino <simone.alocchi@studio.unibo.it>
2026-01-16 18:18:59 +0100

    fixing problems

--- search-objects.php
+++ search-objects.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__ . "/../templates/bootstrap.php"); // assicura $db_obj e session
-require_once(__DIR__ . "/../utils/functions.php");
+require_once(__DIR__ . "/templates/bootstrap.php");
+require_once(__DIR__ . "/utils/functions.php");
 
 $items = $db_obj->getFoundObjects();
 ?>
