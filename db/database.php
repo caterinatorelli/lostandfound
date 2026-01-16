8779c9d1c499d60b197b6d0eeed2b6579fd20836
matteo.crepaldi4 <matteo.crepaldi4@studio.unibo.it>
2026-01-16 15:42:49 +0100

    Moved database files to dedicated folder and refactored database connection management

---
+++ db/database.php
@@ -0,0 +1,46 @@
+<?php
+    class Database {
+        private $db;
+        
+        private function getUser(string $username): array {
+            $query = "SELECT id, password, ruolo FROM utenti WHERE email = ?";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param("s", $username);
+
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function __construct($host, $username, $password, $database, $port) {
+            $this->db = new mysqli($host, $username, $password, $database, $port);
+        }
+
+        /**
+         * Returns the user information
+         * @param string $username user's username
+         * @param string $password user's password
+         * @return array Array containing the user information
+         */
+        public function checkLogin(string $username, string $password): array {
+            $user = $this->getUser($username);
+
+            if (password_verify($password, password_hash($password, PASSWORD_DEFAULT))) {
+                return $user;
+            } else {
+                return array();
+            }
+        }
+
+        public function registerUser(string $username, string $password): void {
+            $password_hash = password_hash($password, PASSWORD_DEFAULT);
+        
+            $query = "INSERT INTO utenti (email, password) VALUES (?, ?)";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param("ss", $username, $password_hash);
+
+            $stmt->execute();
+        }
+    }
+?>
