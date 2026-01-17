0fa9e9c60727858040bcf9c59858b904a0c00790
matteo.crepaldi4 <matteo.crepaldi4@studio.unibo.it>
2026-01-17 16:44:15 +0100

    First implementation of the admin panel

---
+++ admin.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("templates/bootstrap.php");
+    require_once("utils/functions.php");
+
+    $templateParams["titolo"] = "Lost and Found - Admin";
+    $templateParams["nome"] = "components/admin-main.php";
+
+    require_once("templates/base.php");
+?>
