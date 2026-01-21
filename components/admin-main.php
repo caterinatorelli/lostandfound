50c1ba3f69ad33b0d00c8938f2934b082c9e078c
Simollino <simone.alocchi@studio.unibo.it>
2026-01-21 16:55:26 +0100

    minus changes

--- components/admin-main.php
+++ components/admin-main.php
@@ -6,14 +6,16 @@
 <div class="home-wrapper">
     <div class="hero-box">
         <h1>Richieste in sospeso</h1>
-        <h2>ci sono attualmente  <?php echo count($openRequests); ?> richieste in sospeso</h2>
+        <h2>ci sono attualmente  <?php echo count($openRequests); ?> richieste</h2>
 
         <a href="manage-requests.php" class="btn btn-primary">Vai alle richieste</a>
     </div>
 
     <div class="hero-box">
+        <br>
         <h1>Report aperti</h1>
-        <h2>Ci sono attualmente <?php echo count($openCases); ?> report aperti</h2>
+        <h2>Ci sono attualmente <?php echo count($openCases); ?> report</h2>
+        <br>
 
         <a href="search-objects.php" class="btn btn-primary">Vai ai reports</a>
     </div>
